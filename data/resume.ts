fab275ee0aa085439a9bbdec27dbce0430dcb4ef
pranav-k-jha <pranav.jha@mail.concordia.ca>
2025-05-06 20:07:51 -0400

    updated

--- data/resume.ts
+++ data/resume.ts
@@ -108,7 +108,7 @@ const resume = {
       associatedWith: null,
       description:
         "This project aims to securely manage, streamline, and analyze structured and semi-structured YouTube video data based on video categories and trending metrics.",
-      keywords: [
+      skills: [
         "Data Analysis",
         "Cloud Services",
         "Video Analytics",
